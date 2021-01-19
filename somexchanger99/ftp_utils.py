e0a7e42264d3a6e989d376d55d3186669e40994f
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2021-01-19 09:47:51 +0100

    Created FtpClient as manager for ftp connections
    * Created FtpClient extending ftputils.FTPHost class to manage ftp
    connections
    * Now we can walk through directories in mlsd style

--- somexchanger99/ftp_utils.py
+++ somexchanger99/ftp_utils.py
@@ -13,38 +13,40 @@ from pytz.exceptions import AmbiguousTimeError
 logger = logging.getLogger(__name__)
 
 
-class FtpClientNative(ftplib.FTP):
-
-    def listdir(self, path="."):
-        return self.nlst(path)
-
-    def getmtime(self, path):
-        lst_dir_gen = self.mlsd(path)
-        _, file_properties = lst_dir_gen.__next__()
-        return parser.parse(
-            file_properties['modify'], tzinfos={'UTC': tz.gettz('UTC')}
-        )
-
-
-class FtpClientFtpUtil(ftputil.FTPhost):
+class FtpClient(ftputil.FTPHost):
 
     def retrbinary(self, cmd, callback, blocksize=8192, rest=None):
         return self._session.retrbinary(cmd, callback, blocksize, rest)
+
+    def list_dir(self, path='.'):
+        try:
+            for entry in self._native_mlsd(path):
+                yield entry
+        except ftplib.error_perm:
+            yield self._custom_mlsd(path)
+
+    def _native_mlsd(self, path):
+        return self._session.mlsd(path, ['type', 'modify'])
+
+    def _custom_mlsd(self, path):
+        for entry in self.listdir(path):
+            yield (
+                entry,
+                {'type': 'dir' if self.path.isdir(entry) else 'file',
+                 'modify': self.path.getmtime(entry)}
+            )
+
+    def isdir(self, path):
+        return self.path.isdir(path)
 
     def getmtime(self, path):
         return datetime.utcfromtimestamp(self.path.getmtime(path))
 
 
-class FtpClient(object):
-
-    def __init__(self, host, username, password, native=True):
-        return FtpClientNative(host, username, password) if native else FtpClientFtpUtil(host, username, password)
-
-
 class FtpUtils(object):
 
     def __init__(self, host, username, password, base_dir, native=True):
-        self._client = ftputil.FTPHost(host, username, password, native)
+        self._client = FtpClient(host, username, password, native)
         self.base_dir = base_dir
 
     def download_file_content(self, path):
@@ -61,23 +63,21 @@ class FtpUtils(object):
         file_list = []
 
         try:
-            for file_name in self._client.listdir(path):
-                if self._client.path.isdir(file_name):
+            for file_name, file_properties in self._client.list_dir(path):
+                if file_properties['type'] == 'dir':
                     new_path = os.path.join(path, file_name)
-
                     file_list = file_list + self.get_files_to_download(new_path, pattern, date_from)
                 try:
-                    full_path_file = os.path.join(path, file_name)
                     file_date = make_aware(
-                        self._client.getmtime(full_path_file),
-                        date_from.tzinfo
+                        parser.parse(file_properties['modify']), date_from.tzinfo
                     )
                 except AmbiguousTimeError as e:
                     msg = "An error ocurred in date comparation, reason: %s"
                     logger.error(msg, str(e))
                 else:
+                    full_path_file = os.path.join(path, file_name)
                     match_file = re.match(r'{}'.format(pattern), file_name) and \
-                                 file_date >= date_from
+                        file_date >= date_from
                     if match_file:
                         file_list.append((full_path_file, file_name))
         except ftputil.error.FTPError as e:
